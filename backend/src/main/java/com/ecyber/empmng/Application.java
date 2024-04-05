cf02aaf14cb402894111991028bce4f1d1b9a3c3
Randula Mawaththta <randula98@gmail.com>
2024-04-05 14:19:03 +0530

    App Initialized
    Signed-off-by: Randula Mawaththta <randula98@gmail.com>

---
+++ backend/src/main/java/com/ecyber/empmng/Application.java
@@ -0,0 +1,13 @@
+package com.ecyber.empmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
